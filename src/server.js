bf2c77471db457d91d8709fd5f03ede3e24df5f0
Danilo Kasemi <danilo.kasemi@moveoalbania.com>
2019-10-14 10:26:32 +0200

    cors added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var rp = require("request-promise");
+var cors = require('cors');
+
 
 var app = express();
+app.use(cors());
 
 app.listen(9797, () => {
     console.log("Server running on port 9797");
